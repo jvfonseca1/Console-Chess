ebfe9e41832a9915759c60f4ec2105d8612fb2ec
João Victor Fonseca <joao.fonseca.victor@gmail.com>
2023-05-13 19:09:30 -0300

    Changes in Screen Class

--- Console-Chess/Screen.cs
+++ Console-Chess/Screen.cs
@@ -9,18 +9,35 @@ namespace Console_Chess
         {
             for (int i = 0; i < board.Lines; i++)
             {
+                Console.Write (8 - i + " ");
                 for (int j = 0; j < board.Columns; j++)
                 {
-                    if (board.Piece(i,j) == null)
+                    if (board.Piece (i, j) == null)
                     {
                         Console.Write ("- ");
                     }
                     else
                     {
-                        Console.Write (board.Piece (i, j) + " ");
+                        printPiece(board.Piece(i, j));
                     }
                 }
                 Console.WriteLine ();
+            }
+            Console.WriteLine ("  a b c d e f g h");
+        }
+
+        public static void printPiece (Piece piece)
+        {
+            if (piece.Color == Color.White)
+            {
+                Console.Write (piece + " ");
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write (piece + " ");
+                Console.ForegroundColor = aux;
             }
         }
     }
